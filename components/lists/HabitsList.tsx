ea2e74bcd6bed00702dd5239550ef41b1fc8e9f1
Julian Mendonca <julian@capicua.com.uy>
2021-11-27 01:37:40 -0300

    working version

--- components/lists/HabitsList.tsx
+++ components/lists/HabitsList.tsx
@@ -1,6 +1,6 @@
-import { Habit } from '../../pages/history'
 import HabitCard from '../cards/HabitCard'
 import { Flex, FlexProps } from '@chakra-ui/react'
+import { Habit } from '../../src/graphql/autogenerate/schemas'
 type HabitsListProps = FlexProps & {
 	habits: Habit[]
 }
@@ -8,7 +8,7 @@ const HabitsList = ({ habits, ...props }: HabitsListProps) => {
 	return (
 		<Flex direction="column" alignItems="center" width="100%" {...props}>
 			{habits.map((i) => {
-				return <HabitCard key={i.id} habit={i} />
+				return <HabitCard key={i.habit_id} habit={i} />
 			})}
 		</Flex>
 	)
