a19d5b62a431275bf204bef0375ba3aa3ac173c5
Julian Mendonca <julian@capicua.com.uy>
2021-11-30 00:54:04 -0300

    code cleanup

--- hooks/useHabits.ts
+++ hooks/useHabits.ts
@@ -19,14 +19,16 @@ export const useHabits = ({ user, date }: UseHabitsProps) => {
 	const [habits, setHabits] = useState<Habit[]>([])
 
 	const [insertHabitMutation] = useInsertHabitMutation()
-	const { data: habitsData, loading: habitsLoading } = useGetHabitsByUserIdAndDateQuery({
-		variables: {
-			userId: user?.id || 0,
-			date: date.toISOString().split('T')[0],
-			offset: 0
-		}
-	})
 	const [deleteHabitMutation] = useDeleteHabitByPkMutation()
+
+	const getHabitsVariables = {
+		userId: user?.id || 0,
+		date: date.toISOString().split('T')[0]
+	}
+	const { data: habitsData, loading: habitsLoading } = useGetHabitsByUserIdAndDateQuery({
+		variables: getHabitsVariables
+	})
+
 	const insertHabit = (newHabit: string, habitType: Habit_Type_Enum = Habit_Type_Enum.Neutral) => {
 		if (user?.id) {
 			const insertDate = date.toISOString().split('T')[0]
@@ -50,31 +52,20 @@ export const useHabits = ({ user, date }: UseHabitsProps) => {
 				},
 				update: async (cache, { data }) => {
 					const existingHabits = client.readQuery({
-						// The cached query key is the same as the name of the GQL schema
 						query: GetHabitsByUserIdAndDateDocument,
-						variables: {
-							userId: user?.id || 0,
-							date: insertDate,
-							offset: 0
-						}
+						variables: getHabitsVariables
 					})
-					// Now we combine the optimisticResponse we passed in earlier and the existing data
 					const newHabits = [...existingHabits.habit, data?.insert_habit_one]
-					console.log({ existingHabits, habits, newHabits })
-					// Finally we overwrite the cache
 					cache.writeQuery({
 						query: GetHabitsByUserIdAndDateDocument,
-						variables: {
-							userId: user?.id || 0,
-							date: date.toISOString().split('T')[0],
-							offset: 0
-						},
+						variables: getHabitsVariables,
 						data: { habit: newHabits }
 					})
 				}
 			})
 		}
 	}
+
 	const deleteHabit = (habitId: number) => {
 		setHabits((prev) => prev.filter((i) => i.habit_id !== habitId))
 		deleteHabitMutation({
